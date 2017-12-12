6365948f03e33cfd60ca195cabd2bf1cac595b8b
Simone Garuglieri <s.garuglieri@gmail.com>
2017-12-12 11:27:09 +0100

    Working on AddExoenseViewController

---
+++ ManhattanChallengeTest/AddExpenseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TripViewController.swift
+//  ManhattanChallengeTest
+//
+//  Created by Simone Garuglieri on 12/12/2017.
+//  Copyright © 2017 Simone Garuglieri. All rights reserved.
+//
+
+import UIKit
+
+class AddExpenseViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+   
+    @IBAction func tapGesture(_ sender: UITapGestureRecognizer) {
+        
+    }
+    
+}
--- ManhattanChallengeTest/ViewController.swift
+++ ManhattanChallengeTest/ViewController.swift
@@ -64,7 +64,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupLayout()
+//        setupLayout()
     }
     
     
