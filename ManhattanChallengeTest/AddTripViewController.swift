bf39e67bfa9d71c8e43bda0a538abe8b674bf862
Francesca Palese <palesefrancesca@yahoo.com>
2017-12-14 09:20:36 +0100

    Add trip and metrics view

---
+++ ManhattanChallengeTest/AddTripViewController.swift
@@ -0,0 +1,87 @@
+//
+//  AddTripViewController.swift
+//  ManhattanChallengeTest
+//
+//  Created by Francesca Palese on 13/12/2017.
+//  Copyright © 2017 Simone Garuglieri. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+class AddTripViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBAction func saveNewTrip(_ sender: UIBarButtonItem) {
+        if let loc = locationTextField.text {
+        location = loc
+        }
+        if let budgetString = budgetTextField.text {
+            budget = Float(budgetString)
+        }
+        
+        if location != nil && budget != nil {
+            CoreDataController.shared.addTrip(location: location!, budget: budget!)
+        }
+        
+        navigationController?.popViewController(animated: true)
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    @IBOutlet weak var locationTextField: UITextField!{
+        didSet {
+                locationTextField.delegate = self
+        }
+    }
+    
+    @IBOutlet weak var budgetTextField: UITextField!{
+        didSet {
+            budgetTextField.delegate = self
+        }
+    }
+    
+    var location: String?
+    var budget: Float?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+   
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
