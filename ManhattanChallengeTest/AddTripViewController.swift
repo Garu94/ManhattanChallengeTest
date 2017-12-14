a7233c709100dcef8f0fdaa22efdac506fc7b55e
Simone Garuglieri <s.garuglieri@gmail.com>
2017-12-14 10:07:42 +0100

    asfasfd

--- ManhattanChallengeTest/AddTripViewController.swift
+++ ManhattanChallengeTest/AddTripViewController.swift
@@ -8,17 +8,6 @@
 
 import UIKit
 
-extension UIViewController {
-    func hideKeyboardWhenTappedAround() {
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
-        tap.cancelsTouchesInView = false
-        view.addGestureRecognizer(tap)
-    }
-    
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
-    }
-}
 
 class AddTripViewController: UIViewController, UITextFieldDelegate {
     
