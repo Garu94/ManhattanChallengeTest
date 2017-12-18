7945b53d14464b9f2a582c56632368d128c31220
Francesca Palese <palesefrancesca@yahoo.com>
2017-12-18 16:57:28 +0100

    storyboard changes

---
+++ ManhattanChallengeTest/UIButton+shadow.swift
@@ -0,0 +1,62 @@
+//
+//  UIButton+shadow.swift
+//  ManhattanChallengeTest
+//
+//  Created by Francesca Palese on 18/12/2017.
+//  Copyright © 2017 Simone Garuglieri. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+@IBDesignable
+class DesignableButton: UIButton {
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
+    }
+}
