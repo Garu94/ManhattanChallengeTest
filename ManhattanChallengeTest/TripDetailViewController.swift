2ce6829c1c6694e11bbebc1df081c1caa1bb33c4
Simone Garuglieri <s.garuglieri@gmail.com>
2017-12-18 09:31:27 +0100

    some changes

---
+++ ManhattanChallengeTest/TripDetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  TripDetailViewController.swift
+//  ManhattanChallengeTest
+//
+//  Created by Simone Garuglieri on 18/12/2017.
+//  Copyright © 2017 Simone Garuglieri. All rights reserved.
+//
+
+import UIKit
+
+class TripDetailViewController: UIViewController {
+    
+    var trip: Trip?
+    var total = 0
+    
+    
+    @IBOutlet weak var totalExpenseLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
